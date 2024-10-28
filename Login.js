aa2d725a482ca63897bfde375427b8836f8567a2
Shashavali <109945362+Shashavali-11@users.noreply.github.com>
2024-10-28 11:20:12 +0530

    Add files via upload

---
+++ Login.js
@@ -0,0 +1,17 @@
+var user = "Shashavali"
+var psw = "Shasha"
+
+if(user=="Shashavali" && psw=="Shasha"){
+    console.log("Successfully LogedIn")
+}else if(user=="Shashavali" && psw!="Shasha"){
+    console.log("Password is Invalid...")
+}else if(user!="Shashavali" && psw=="Shasha"){
+    console.log("USer is Invalid...")
+}else{
+    console.log("Invalid Credientials.")
+}
+
+
+
+
+
